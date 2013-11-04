24a61740dd9bb4042e273b9b1f64b2e4b19cf1ec
Greg <greg@yhathq.com>
2013-11-04 12:25:42 -0500

    added yhat functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+# Beer Rec in `ruby`
+
+## Quickstart
+
+    # in one window
+    $ ruby hello.rb
+    # in another window
+    $ curl -s http://localhost:4567/ | jq .
--- hello.rb
+++ hello.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'yhat'
+require 'json'
 
+yh = Yhat.new("greg", "testing123", "http://starphleet-aa02a554-1463294931.us-west-1.elb.amazonaws.com/")
 get '/' do
-    "Hello World!"
+  content_type :json
+  # beer = params[:name]
+  beer = "Coors Light"
+  yh.predict("PydataBeerRec", { "beer" => beer }.to_json).to_json
 end
